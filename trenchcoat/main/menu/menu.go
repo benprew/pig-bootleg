31bf2047c0d1920631a545e11c601fb5c6151e8e
Ben Prew <ben@throwingbones.com>
2018-09-05 21:55:02 -0700

    Add menu, and reset game when you die

---
+++ trenchcoat/main/menu/menu.go
@@ -0,0 +1,70 @@
+components {
+  id: "menu"
+  component: "/main/menu/menu.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "intro"
+  component: "/main/menu/menu.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "title"
+  type: "sprite"
+  data: "tile_set: \"/main/menu/title.tilesource\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 640.0
+    y: 360.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "instructions"
+  type: "sprite"
+  data: "tile_set: \"/main/menu/instructions.tilesource\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 640.0
+    y: 360.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
